fe83281fcffbb57acca913891fc2421f3c061593
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-23 10:30:03 -0500

    Add solution

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [x for x in num_list if x % 2 == 0]
+    return even_list
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list = [ string + "!" for string  in sentence_list]
+    return sentence_list
